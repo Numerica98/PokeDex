d38ae77ebbf970a899c92296a188f74b5c21094e
Numerica98 <00041517@uca.edu.sv>
2019-04-05 17:22:03 -0600

    Paquetes y clase pokemon creadas

---
+++ app/src/main/java/com/numerica98/pokedex/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.numerica98.pokedex.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.numerica98.pokedex.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/numerica98/pokedex/models/Pokemon.kt
@@ -0,0 +1,8 @@
+package com.numerica98.pokedex.models
+
+data class Pokemon {
+    val abilities:String= "N/A"
+    val forms:String="N/A"
+    val game_indices:String= "N/A"
+
+}
